ced43d5f1f3a65a04f69bb1e2ece874cf9857b07
James Dong <45552485+Dongongngg@users.noreply.github.com>
2020-10-23 17:41:47 +1100

    delete file

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-
-import "./App.css";
+//import module
+import Configurator from "./modules/AdWordsConfig/Configurator";
+//
+import "./app.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">asd</header>
+      <Configurator />
     </div>
   );
 }
---
+++ client/src/modules/AdWordsConfig/Configurator.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdWordsConfig = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Test</header>
+    </div>
+  );
+};
+
+export default AdWordsConfig;
